6fcdc2de568c9586c4ff6cb950e11521b4bb7ee6
hoannhan2003 <nguyenhoannhan.nnn@gmail.com>
2024-09-07 00:03:59 +0700

    add3

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
-var typed = new Typed('.text', {
+var typed = new Typed('.text1', {
     strings: ["Frontend Development ", "Web Developer"],
     typeSpeed: 50,
     backSpeed: 50,
     backDelay: 1000,
     loop: true
   });
+
+  function showHide() {
+    var moreText = document.getElementById("more-text");
+    var button = document.querySelector("button");
+    
+    if (moreText.style.display === "none") {
+        moreText.style.display = "inline";
+        button.innerHTML = "<b>Less About Me</b>";
+    } else {
+        moreText.style.display = "none";
+        button.innerHTML = "<b>More About Me</b>";
+    }
+}
